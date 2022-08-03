3540b114c2aa61ef81c68aee2ce62f955d657e8c
Davi Vilarinho <davi.r.vilarinho@gmail.com>
2022-08-03 00:24:07 -0300

    can shutdown without bugging everything

--- shutdown_kernel_module.c
+++ shutdown_kernel_module.c
@@ -1,23 +1,15 @@
 #include <asm/io.h>
-#include <linux/delay.h>
 #include <linux/interrupt.h>
-#include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/notifier.h>
-#include <linux/reboot.h>
-#include <linux/sched.h>
-#include <linux/workqueue.h>
 
 #define KBD_IRQ 1
 
 static struct work_struct check_shutdown_condition_task;
 
 static unsigned char codigo_da_tecla_da_controladora_de_teclado;
-static unsigned char kbd_64;
 static unsigned char pressionado = 0;
 
 static void check_shutdown_condition(struct work_struct *w) {
-  printk("%i", codigo_da_tecla_da_controladora_de_teclado);
   switch (codigo_da_tecla_da_controladora_de_teclado) {
   case 51:;
   case 179:
@@ -26,7 +18,7 @@ static void check_shutdown_condition(struct work_struct *w) {
   case 52:
   case 180:
     if (pressionado) {
-      printk("shutdown");
+      printk("shutdown detectado");
       kernel_power_off();
     }
     break;
@@ -37,11 +29,8 @@ static void check_shutdown_condition(struct work_struct *w) {
 
 static irqreturn_t interrupcao_teclado(int irq, void *dev_id) {
   codigo_da_tecla_da_controladora_de_teclado = inb(0x60);
-  kbd_64 = inb(0x64);
 
-  //  flush_workqueue(&check_shutdown_condition_task);
   queue_work(system_long_wq, &check_shutdown_condition_task);
-  //  schedule_work(&check_shutdown_condition_task);
 
   return IRQ_HANDLED;
 }
